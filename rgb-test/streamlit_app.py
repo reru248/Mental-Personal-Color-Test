3bbaee86386e520baf37e6a1bc748a17f465377d
reru248 <tmero1234@gmail.com>
2025-08-28 17:31:47 +0900

    Update streamlit_app.py

--- rgb-test/streamlit_app.py
+++ rgb-test/streamlit_app.py
@@ -21,7 +21,7 @@ st.markdown(
         background-color: #f0f2f6; 
         margin-bottom: 1rem; 
         flex-direction: column;
-        font-size: 1.5rem;
+        font-size: 1rem;
     }
     .button-group {
         display: flex;
@@ -297,5 +297,6 @@ if question_list and description_blocks:
         if st.button("다시 검사하기"):
             st.session_state.clear()
             st.rerun()
+
 
 
